e135cea5d7637ae79ff814df3a632f4f7c290e64
2014005123 <2014005123@haynag.ac.kr>
2018-03-16 16:50:42 +0900

    feat: check argument for select mode

--- proj_shell/src/shell.c
+++ proj_shell/src/shell.c
@@ -1,6 +1,24 @@
 
-void
-main(void) {
+/**
+ * this file is simple user-level unix shell
+ *
+ * @author Dujin Jeong
+ * @since  2018-03-16
+ */
 
+#include <stdio.h>
 
+int
+main(int argc, char *argv[]) {
+	
+	if (argc < 2) {
+		// shell mode
+	} else if (argc == 2) {
+		// batch mode
+	} else {
+		printf("ERR : Too many argument\n");
+		printf("shell mode : usage: ./shell\n");
+		printf("batch mode : usage: ./shell [batchfile]\n");
+	}
+	return 0;
 }
