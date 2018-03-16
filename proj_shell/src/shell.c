e9d8ce6e11d9b552d63ac8a39841534351f60520
2014005123 <2014005123@haynag.ac.kr>
2018-03-16 17:18:33 +0900

    feat: make functions for mode
    Interactive Mode, Batch Mode

--- proj_shell/src/shell.c
+++ proj_shell/src/shell.c
@@ -8,17 +8,34 @@
 
 #include <stdio.h>
 
+void InteractiveMode();
+void BatchMode(char *path);
+
 int
 main(int argc, char *argv[]) {
 	
 	if (argc < 2) {
-		// shell mode
+		InteractiveMode();
 	} else if (argc == 2) {
-		// batch mode
+		BatchMode(argv[1]);
 	} else {
 		printf("ERR : Too many argument\n");
 		printf("shell mode : usage: ./shell\n");
 		printf("batch mode : usage: ./shell [batchfile]\n");
 	}
+
 	return 0;
 }
+
+void
+InteractiveMode(void) {
+	while (1) {
+		printf("prompt> ");
+	}
+}
+
+void
+BatchMode(char *path) {
+	printf("%s\n", path);	
+}
+
