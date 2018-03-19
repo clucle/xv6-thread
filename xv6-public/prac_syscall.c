200e6d67fe4532b10a1b97acc2ac5b139993a5f5
2014005123 <2014005123@haynag.ac.kr>
2018-03-20 02:35:18 +0900

    feat: works my_userapp

---
+++ xv6-public/prac_syscall.c
@@ -0,0 +1,21 @@
+#include "types.h"
+#include "defs.h"
+
+// Simple system call
+int
+printk_str(char *str)
+{
+	cprintf("%s\n", str);
+	return 0xABCDABCD;
+}
+
+int
+sys_myfunction(void)
+{
+	char *str;
+	// Decode argument using argstr
+	if (argstr(0, &str) < 0) {
+		return -1;
+	}
+	return printk_str(str);
+}
